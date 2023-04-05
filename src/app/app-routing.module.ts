559b5a87b548be6eafe39fb7957ae66675f4f724
Mihir Deshpande <mihir.v.deshpande@outlook.com>
2023-04-05 17:47:00 -0400

    add bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SignUpComponent} from "./sign-up/sign-up.component";
+import {SignInComponent} from "./sign-in/sign-in.component";
 
-const routes: Routes = [];
 
+const routes: Routes = [
+  {path:'about', component:SignUpComponent},
+  {path:'product', component:SignInComponent}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
