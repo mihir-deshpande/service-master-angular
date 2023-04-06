41ff21500c420805a1bb8f3e8fccebf3ed84070f
Mihir Deshpande <mihir.v.deshpande@outlook.com>
2023-04-05 20:39:36 -0400

    add sign-up and sign-in forms templates

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-sign-in',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent {
+  signInForm = new FormGroup({
+    email: new FormControl<string>('', [Validators.required, Validators.email]),
+    password: new FormControl<string>('', [Validators.required])
+  })
+
+  signIn() {
+    if (this.signInForm.valid) {
+      alert(JSON.stringify(this.signInForm.value));
+      alert("Signed In");
+    } else {
+      alert("Invalid form")
+    }
+  }
 
 }
