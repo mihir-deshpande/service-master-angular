7043c10e0e6c1f4b8d12f3473d55bac057d96833
Mihir Deshpande <mihir.v.deshpande@outlook.com>
2023-04-06 19:51:41 -0400

    navigation refresh not working yet.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
-import {AuthService} from "../services/auth.service";
-
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
   navbarCollapsed = true;
   isLoggedIn = false;
   userType!: 'ADMIN' | 'CUSTOMER' | 'PROVIDER';
