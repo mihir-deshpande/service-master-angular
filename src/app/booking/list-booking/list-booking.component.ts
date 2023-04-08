5b270c89eedf97acfbbb24b186895b7f0d07efe6
Mihir Deshpande <mihir.v.deshpande@outlook.com>
2023-04-07 23:29:12 -0400

    list booking

--- src/app/booking/list-booking/list-booking.component.ts
+++ src/app/booking/list-booking/list-booking.component.ts
@@ -8,10 +8,27 @@ import { BookingService, Booking } from '../../services/booking.service';
 })
 export class ListBookingComponent implements OnInit {
   bookings: Booking[] = [];
+  newDates: { [key: string]: string } = {};
+
+  rescheduleVisible: { [key: string]: boolean } = {};
 
   constructor(private bookingService: BookingService) { }
 
   ngOnInit(): void {
+    this.fetchBookings();
   }
 
+  fetchBookings(): void {
+    const userType = localStorage.getItem('user_type') || '';
+    this.bookingService.getBookingsByUserType(userType)
+      .subscribe(bookings => this.bookings = bookings);
+  }
+
+  handleDelete(id: string): void {
+    this.bookingService.deleteBooking(id).subscribe(() => this.fetchBookings());
+  }
+
+  handleReschedule(id: string, newDate: string): void {
+    this.bookingService.updateBookingDate(id, newDate).subscribe(() => this.fetchBookings());
+  }
 }
